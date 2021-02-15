947d36bd65df6bcb1e7869453718ab1eff5f2c4d
Rohit Kumar <75325602+Rohit-Alex@users.noreply.github.com>
2021-02-15 18:41:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from "react";
-
-const App = () => {
-  const [timer, setTimer] = useState(new Date().toLocaleTimeString());
-
-  useEffect(() => {
-    setInterval(() => {
-      setTimer(new Date().toLocaleTimeString());
-    }, 1000);
-    return () => {
-      clearInterval(timer);
-    };
-  });
-
-  return (
-    <div className="Clock">
-      <h3 id="time">{timer}</h3>
-    </div>
-  );
-};
+import React, { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    this.timer = setInterval(() => this.setState({ date: new Date() }), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.date.toLocaleTimeString()}</h3>
+      </div>
+    );
+  }
+}
 
 export default App;
